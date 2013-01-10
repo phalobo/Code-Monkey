12ad8067396dadb399808a3e75f59f55b7738b8d
Ian Smith <ian@crunchbang.(none)>
2013-01-10 17:40:54 -0600

    add rails project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeMonkey::Application.config.secret_token = '4eba47b2ef1e8ba43cacd87a4b59dd454a621477527ca555e9b6980e3b5ca842cb6954ed1d9f9fa152562cb5cb05a74debdd978184e27e921a1a823c48a28f71'
